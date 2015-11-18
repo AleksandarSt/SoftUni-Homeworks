ddf0a1799c3bf39a1c923baa4af3a14e3b0cac77
AleksandarSt <A_V_S_@abv.bg>
2015-11-19 00:56:22 +0200

    Computer Catalog new classes added

---
+++ OOP Defining Classes/ComputerCatalog/Computer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerCatalog
+{
+    class Computer
+    {
+        private string name;
+        private Component graphicCard;
+        private Component cpu;
+        private Component motherboard;
+        private Component ram;
+        private Component storage;
+        private decimal price;
+
+        #region Properties
+
+        public string Name 
+        {
+            get { return this.name; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name must not be null or empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Price 
+        {
+            get { return this.price; }
+            set 
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Price cannot be negative!");
+                }
+
+                this.price = value; 
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+
+
+        #endregion
+    }
+}
