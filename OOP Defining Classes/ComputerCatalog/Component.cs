3db441dcf917f83fe323226dcda23fef639de477
AleksandarSt <A_V_S_@abv.bg>
2015-11-19 00:58:14 +0200

    Computer Catalog new classes added

---
+++ OOP Defining Classes/ComputerCatalog/Component.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerCatalog
+{
+    class Component
+    {
+        private string name;
+        private string details;
+        private decimal price;
+
+        #region Properties
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name must not be null or empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public string Details
+        {
+            get { return this.details; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name must not be null or empty");
+                }
+                this.details = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative!");
+                }
+
+                this.price = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Component(string name, string details, decimal price)
+        {
+            this.Name = name;
+            this.Details = details;
+            this.Price = price;
+        }
+
+        public Component(string name, decimal price)
+            :this(name,null,0)
+        {
+ 
+        }
+
+        #endregion
+    }
+}
