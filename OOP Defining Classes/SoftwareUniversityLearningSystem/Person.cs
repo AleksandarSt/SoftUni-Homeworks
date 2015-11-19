b271481a17b9091015f31fa7bda609fde9ab7783
AleksandarSt <A_V_S_@abv.bg>
2015-11-19 18:19:17 +0200

    SULS Added some classes

---
+++ OOP Defining Classes/SoftwareUniversityLearningSystem/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareUniversityLearningSystem
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        #region Properties
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("First name cannot be null or empty.");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("First name cannot be null or empty.");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value<0||string.IsNullOrEmpty(value.ToString()))
+                {
+                    throw new ArgumentException("Person Age cannot be null, empty or negative!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public Person()
+            : this(null, null, 0)
+        {
+            
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
