f072b62cb9f7aaa1165768e511ab9565e5bbe77e
AleksandarSt <A_V_S_@abv.bg>
2015-11-23 15:13:32 +0200

    Exception Handling Homework added

---
+++ OOP-Exception-Handling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        const int inputNumbers=10;
+
+        public static void ReadNumber(int start, int end,int counter)
+        {
+            int number=0;
+            Console.WriteLine("Enter number in range [{0}..{1}]", start, end);
+
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number >= start && number <= end)
+                    {
+                        Console.WriteLine("Correct input.Number {0} is in range [{1}..{2}]", number, start, end);
+                        counter++;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Number" + number + "is out of the range [" + start + ".." +
+                                                              end + "]");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid number.");
+
+
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                Console.WriteLine("Number {2} is out of the range [{0}..{1}]\nPlease enter valid number[{0}..{1}] this time...", start, end,number);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number.\nPlease enter valid number[{0}..{1}] this time...",start,end);
+            }
+            finally
+            {
+                if (counter < inputNumbers)
+                {
+                    ReadNumber(start, end, counter);
+                }
+                else
+                {
+
+                    Console.WriteLine("End of program!");
+                }
+                
+            }
+
+        }
+
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+            int counter = 0;
+
+            ReadNumber(start,end,counter);
+        }
+    }
+}
---
+++ OOP-Exception-Handling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Number should be positive or 0.");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Number cannot be negative.");
+            }
+            catch (ArgumentException)
+            {
+
+                Console.WriteLine("Invalid number.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+        }
+    }
+}
