993ac55c50f74ff27115e64b948688865102b4b7
AleksandarSt <A_V_S_@abv.bg>
2015-12-05 20:51:35 +0200

    Other Types
    Other Types

---
+++ OOP-Other-Types/GalacticGPS/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticGPS
+{
+    public struct Location
+    {
+        private double latitude;
+        private double longitude;
+    }
+}
