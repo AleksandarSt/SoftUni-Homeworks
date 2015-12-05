5b3155d24f0e3411445f57c341a41d31c7c22365
AleksandarSt <A_V_S_@abv.bg>
2015-12-05 23:06:04 +0200

    Galactic GPS added

--- OOP-Other-Types/GalacticGPS/GalacticGps.cs
+++ OOP-Other-Types/GalacticGPS/GalacticGps.cs
@@ -10,6 +10,9 @@ namespace GalacticGPS
     {
         static void Main(string[] args)
         {
+            Location home = new Location(18.037986, 28.870097, Planet.Earth);
+            Console.WriteLine(home);
+
         }
     }
 }
--- OOP-Other-Types/GalacticGPS/Location.cs
+++ OOP-Other-Types/GalacticGPS/Location.cs
@@ -8,7 +8,54 @@ namespace GalacticGPS
 {
     public struct Location
     {
+        const double maxLatitude = 90;
+        const double maxLongtitude = 180;
+
         private double latitude;
         private double longitude;
+        private Planet planet;
+
+        public Location(double latitude, double longitude,Planet planet)
+            :this()
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Planet = planet;
+        }
+
+        public double Latitude
+        {
+            get { return this.latitude; }
+            set
+            {
+                if (value<0 ||value>maxLatitude)
+                {
+                    throw new ArgumentOutOfRangeException("Latitude must be in range 0 to 90");
+                }
+
+                this.latitude=value;
+            }
+        }
+
+        public double Longitude {
+            get { return this.longitude; }
+            set
+            {
+                if (value<0 ||value>maxLongtitude)
+                {
+                    throw new ArgumentOutOfRangeException("Longtitude must be in range 0 to 180");
+                }
+
+                this.longitude = value;
+            }
+        }
+
+        public Planet Planet { get; set; }
+
+        public override string ToString()
+        {
+            string result = this.Latitude.ToString() + ", " + this.Longitude.ToString() + " - " + this.Planet;
+            return result;
+        }
     }
 }
