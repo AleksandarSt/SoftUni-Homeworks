ce74c1ebb60917e64024fd1fb8d8f5fb30abaf70
AleksandarSt <A_V_S_@abv.bg>
2015-12-08 01:26:16 +0200

    The Slum items added

---
+++ OOP-Encapsulation-Polymorphism/TheSlum-Skeleton/Models/Items/Pill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSlum.Models.Items
+{
+    class Pill:Bonus
+    {
+        private const int PillHealthEffect = 100;
+        private const int PillDefenseEffect = 0;
+        private const int PillAttackEffect = 0;
+
+        public Pill(string id)
+            : base(id, PillHealthEffect, PillDefenseEffect, PillAttackEffect)
+        {
+            this.Timeout = 1;
+            this.Countdown = 1;
+            this.HasTimedOut = false;
+        }
+    }
+}
