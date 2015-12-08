4feaac0d79d9e1bc2ae3eed185db9a6a343c0641
AleksandarSt <A_V_S_@abv.bg>
2015-12-08 14:14:35 +0200

    Additional engine
    Additional engine

---
+++ OOP-Encapsulation-Polymorphism/TheSlum-Skeleton/GameEngine/AdditionalEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSlum.GameEngine
+{
+    class AdditionalEngine:Engine
+    {
+        protected override void ExecuteCommand(string[] inputParams)
+        {
+            switch (inputParams[0])
+            {
+                case "status":
+                    this.PrintCharactersStatus(this.characterList);
+                    break;
+                case "create":
+                    this.CreateCharacter(inputParams);
+                    break;
+                case "add":
+                    this.AddItem(inputParams);
+                    break;
+            }
+        }
+
+        protected override void CreateCharacter(string[] inputParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddItem(string[] inputParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
