86a7481441f3c16c567b92be99fd6ee3cfe53f4d
AleksandarSt <A_V_S_@abv.bg>
2015-12-12 16:21:27 +0200

    Interest calculator added

---
+++ OOP-Delegates-Events/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator
+{
+
+    public delegate decimal CalculateInterest(decimal sum, decimal interest, decimal years);
+
+    public class InterestCalculator
+    {
+        public InterestCalculator(decimal money, decimal interest, decimal years, CalculateInterest interestMethod)
+        {
+            this.Money = money;
+            this.Interest = interest;
+            this.Years = years;
+            this.InterestMethod = interestMethod;
+        }
+
+        public decimal Money { get; set; }
+
+        public decimal Interest { get; set; }
+
+        public decimal Years { get; set; }
+
+        public CalculateInterest InterestMethod { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Money = {0}\nInterest = {1}\nYears = {2}\nTotalSum = {3:F4}\n",
+                this.Money,
+                this.Interest,
+                this.Years,
+                this.InterestMethod(Money,Interest,Years));
+        }
+    }
+}
---
+++ OOP-Delegates-Events/InterestCalculator/MainProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator
+{
+    class MainProgram
+    {
+        private const int periods=12;
+
+        static void Main()
+        {
+            var compound = new InterestCalculator(500, 5.6m, 10, GetCompoundInterest);
+            Console.WriteLine(compound);
+
+            var simple = new InterestCalculator(2500, 7.2m, 15, GetSimpleInterest);
+            Console.WriteLine(simple);
+        }
+
+        public static decimal GetSimpleInterest(decimal sum, decimal interest, decimal years)
+        {
+            decimal result = sum * (1 + ((interest / 100) * years));
+            return result;
+        }
+
+        public static decimal GetCompoundInterest(decimal sum, decimal interest, decimal years)
+        {
+            decimal result = sum * (decimal)Math.Pow((double)(1 + ((interest/100) / periods)), (double)years * periods);
+            return result;
+        }
+    }
+}
