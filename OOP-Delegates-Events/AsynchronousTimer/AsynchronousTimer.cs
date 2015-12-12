43b562c910852f8b6f03b098db7e7cf3a4225326
AleksandarSt <A_V_S_@abv.bg>
2015-12-12 17:19:33 +0200

    Asynchronous Timer added

---
+++ OOP-Delegates-Events/AsynchronousTimer/AsynchronousTimer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace AsynchronousTimer
+{
+    class AsynchronousTimer
+    {
+        private Action<DateTime> action;
+        private int ticks;
+        private int timeIntervals;
+
+        public AsynchronousTimer(Action<DateTime> action, int ticks, int timeIntervals)
+        {
+            this.Action = action;
+            this.Ticks = ticks;
+            this.TimeIntervals = timeIntervals;
+        }
+
+        public Action<DateTime> Action { get; set; }
+
+        public int Ticks { get; set; }
+
+        public int TimeIntervals { get; set; }
+
+        public void Run()
+        {
+            var parallel = new Thread(this.Execute);
+            parallel.Start();
+        }
+
+        private void Execute()
+        {
+            for (int i = 0; i < this.Ticks; i++)
+            {
+                Thread.Sleep(this.TimeIntervals);
+                this.Action(DateTime.Now);
+            }
+        }
+    }
+}
---
+++ OOP-Delegates-Events/AsynchronousTimer/MainProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousTimer
+{
+    class MainProgram
+    {
+        static void Main()
+        {
+            AsynchronousTimer asynchronousTimer = new AsynchronousTimer(PrintDateTimeMilliseconds, 10, 1000);
+            asynchronousTimer.Run();
+        }
+
+        static void PrintDateTimeMilliseconds(DateTime nowDateTime)
+        {
+            Console.WriteLine(
+                "{0}:{1}:{2}.{3}",
+                nowDateTime.Hour,
+                nowDateTime.Minute,
+                nowDateTime.Second,
+                nowDateTime.Millisecond);
+        }
+    }
+}
