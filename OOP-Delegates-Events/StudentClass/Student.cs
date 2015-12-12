93ceeb3fd3b14aabfc979541bff0d4db1f602a2a
AleksandarSt <A_V_S_@abv.bg>
2015-12-12 17:52:09 +0200

    StudentClass added

---
+++ OOP-Delegates-Events/StudentClass/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StudentClass
+{
+    class Student
+    {
+        private string name;
+        private int age;
+
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public event PropertyChanged;
+    }
+}
