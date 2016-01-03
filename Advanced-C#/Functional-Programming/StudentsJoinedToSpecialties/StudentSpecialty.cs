b98ded755268ac828ebcb2cca92c491c0102b085
AleksandarSt <A_V_S_@abv.bg>
2016-01-04 00:31:33 +0200

    Group and join added

---
+++ Advanced-C#/Functional-Programming/StudentsJoinedToSpecialties/StudentSpecialty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsJoinedToSpecialties
+{
+    public class StudentSpecialty
+    {
+
+        public StudentSpecialty(string specialtyName, string facNum)
+        {
+            this.SpecialtyName = specialtyName;
+            this.FacNum = facNum;
+        }
+
+        public string SpecialtyName { get; set; }
+
+        public string FacNum { get; set; }
+    }
+}
