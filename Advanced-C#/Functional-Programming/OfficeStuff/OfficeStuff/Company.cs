38ad0479b81469ca74bc3ba48f2ccabe747758b6
AleksandarSt <A_V_S_@abv.bg>
2016-01-04 19:26:51 +0200

    OFfice Stuff added

---
+++ Advanced-C#/Functional-Programming/OfficeStuff/OfficeStuff/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeStuff
+{
+    public class Company
+    {
+
+        public Company(string companyName, int amount, string product)
+        {
+            this.CompanyName = companyName;
+            this.Amount = amount;
+            this.Product = product;
+        }
+
+        public string CompanyName { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Product { get; set; }
+    }
+}
---
+++ Advanced-C#/Functional-Programming/OfficeStuff/OfficeStuff/OfficeStuff.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeStuff
+{
+    class OfficeStuff
+    {
+        static void Main()
+        {
+
+            int numberOfCompanies = int.Parse(Console.ReadLine());
+
+            List<Company> companies = new List<Company>();
+
+            char[] separator = {'-', '|', ' '};
+
+            for (int i = 0; i < numberOfCompanies; i++)
+            {
+                string[] tokens=Console.ReadLine().Split(separator,StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = tokens[0];
+                int amount = int.Parse(tokens[1]);
+                string product = tokens[2];
+
+                companies.Add
+                    (
+                    new Company(companyName,amount,product)
+                    );
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numberOfCompanies; i++)
+            {
+                Console.WriteLine(companies[i]);
+            }
+        }
+    }
+}
