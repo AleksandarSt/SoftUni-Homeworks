e47e8d79ed1094fa783e97c33d2fd8980d99c819
AleksandarSt <A_V_S_@abv.bg>
2016-01-08 00:07:55 +0200

    Event updated

---
+++ High-Quality-Code/Code-Formatting/Event/MultiDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formatting
+{
+    class MultiDictionary<T1, T2>
+    {
+        public MultiDictionary(bool isEvent)
+        {
+
+        }
+
+        internal void Add(string p, Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Remove(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ High-Quality-Code/Code-Formatting/Event/OrderedBag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formatting
+{
+    class OrderedBag<T>
+    {
+        internal void Add(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
