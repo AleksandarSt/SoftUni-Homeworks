bbb8a601224cf38266937b32c156dc759285a727
AleksandarSt <A_V_S_@abv.bg>
2016-01-10 01:56:54 +0200

    Code documentation added

---
+++ High-Quality-Code/Code-Documentation-Comments/Code-Documentation-Comments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Code_Documentation_Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "Some text";
+            var result = input.ToMd5Hash();
+            Console.WriteLine(input.ToByteArray());
+        }
+    }
+}
