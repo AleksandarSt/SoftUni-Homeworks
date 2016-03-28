b2218597030044f8df2b3f81972061df56f5758b
AleksandarSt <A_V_S_@abv.bg>
2016-03-29 00:30:38 +0300

    seriesofLetters

---
+++ Advanced-C#/Regular-Expressions/Series-of-letters/SeriesOfLetters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Series_of_letters
+{
+    class SeriesOfLetters
+    {
+        static void Main()
+        {
+
+            /*Write a program that reads a string 
+             * from the console and replaces all series of consecutive identical letters with a single one.*/
+
+            var text = Console.ReadLine();
+            string pattern = @"(.)\1+";
+            var regex = new Regex(pattern);
+            Console.WriteLine(regex.Replace(text, "$1"));
+
+        }
+    }
+}
